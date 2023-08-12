a7b43c4da7d45893eaa0d800a8c9729874468f5a
JulinhoTomazBrito <julinhotomazbrito@gmail.com>
2023-08-12 15:22:33 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/imobApp/imobAppWeb/ImobAppWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.imobApp.imobAppWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobAppWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/imobApp/imobAppWeb/ImobAppWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imobApp.imobAppWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobAppWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
