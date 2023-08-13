a5278dfa28257c53cb4bc9e35bb50d8fc9d776d2
JulinhoTomazBrito <julinhotomazbrito@gmail.com>
2023-08-13 15:22:23 -0300

    Inclusão do MapStruct ao projeto

---
+++ src/main/java/br/com/imobApp/imobAppWeb/comum/mappers/ImobiliariaMapper.java
@@ -0,0 +1,18 @@
+package br.com.imobApp.imobAppWeb.comum.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import br.com.imobApp.imobAppWeb.dtos.ImobiliariaDTO;
+import br.com.imobApp.imobAppWeb.models.EntImobiliaria;
+
+
+
+@Mapper
+public interface ImobiliariaMapper {
+	
+	ImobiliariaMapper INSTANCE = Mappers.getMapper(ImobiliariaMapper.class);
+    ImobiliariaDTO toDTO(EntImobiliaria cliente);
+    EntImobiliaria toEnt(ImobiliariaDTO clienteDTO);
+    
+}
---
+++ src/main/java/br/com/imobApp/imobAppWeb/comum/mappers/ImovelMapper.java
@@ -0,0 +1,16 @@
+package br.com.imobApp.imobAppWeb.comum.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import br.com.imobApp.imobAppWeb.dtos.ImovelDTO;
+import br.com.imobApp.imobAppWeb.models.EntImovel;
+
+@Mapper
+public interface ImovelMapper {
+	
+	ImovelMapper INSTANCE = Mappers.getMapper(ImovelMapper.class);
+	ImovelDTO entToDTO(EntImovel entImovel);
+    EntImovel dtoToEnt(ImovelDTO imovelDTO);
+    
+}
